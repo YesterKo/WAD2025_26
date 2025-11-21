73df14509241399e53696349eca53e76712f8c93
Jesper Korsen <jesper.korsen@gmail.com>
2025-11-21 22:33:41 +0200

    Set and fix stuff to see that the view is working

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import PostsView from "../views/PostsView.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "posts",
+    component: PostsView,
   },
   {
     path: "/about",
--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,25 +1,30 @@
-<component>
-<div class="responsive-layout">
-  <aside class="left-aside">
-    <p>This is the left aside.</p>
-  </aside>
+<template>
+  <div class="responsive-layout">
+    <aside class="left-aside">
+      <p>This is the left aside.</p>
+    </aside>
 
-<!-- TODO: add logic to use post component and populate it -->
-  <main>
-      <PostComponent v-for="post in posts">
-  </main>
+    <!-- TODO: add logic to use post component and populate it -->
+    <main>
+      <!-- eslint-disable-next-line -->
+      <PostComponent v-for="post in posts" />
+    </main>
 
-  <aside class="right-aside">
-    <p>This is the right aside.</p>
-  </aside>
-</div>
-</component>
+    <aside class="right-aside">
+      <p>This is the right aside.</p>
+    </aside>
+  </div>
+</template>
 <script>
+import PostComponent from "@/components/PostComponent.vue";
 export default {
   computed: {
     posts() {
       return this.$store.state.posts;
     },
+  },
+  components: {
+    PostComponent,
   },
 };
 </script>
