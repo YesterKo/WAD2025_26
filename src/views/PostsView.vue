03a3eb35bf5194bc0e8de9c245ffe53ef418a0d8
Jesper Korsen <jesper.korsen@gmail.com>
2025-11-21 22:13:22 +0200

    Add post populating

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,12 +1,25 @@
+<component>
 <div class="responsive-layout">
   <aside class="left-aside">
     <p>This is the left aside.</p>
   </aside>
 
 <!-- TODO: add logic to use post component and populate it -->
-  <main id="posts-container"></main>
+  <main>
+      <PostComponent v-for="post in posts">
+  </main>
 
   <aside class="right-aside">
     <p>This is the right aside.</p>
   </aside>
 </div>
+</component>
+<script>
+export default {
+  computed: {
+    posts() {
+      return this.$store.state.posts;
+    },
+  },
+};
+</script>
