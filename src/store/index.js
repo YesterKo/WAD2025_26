d8c18e6aeb2dd9b6e447af2b647d6ccd0e9e55d0
Jesper Korsen <jesper.korsen@gmail.com>
2025-11-21 20:03:00 +0200

    Add posts to vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,80 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    posts: [
+      {
+        authorImage: "img/pfp1.jpg",
+        date: "Oct 3, 2025",
+        image: "img/food.jpg",
+        text: "Jippie food hihi!",
+        likes: 0,
+      },
+      {
+        authorImage: "img/pfp1.jpg",
+        date: "Oct 3, 2025",
+        image: null,
+        text: "What are bananas made of?",
+        likes: 0,
+      },
+      {
+        authorImage: "img/pfp1.jpg",
+        date: "Oct 3, 2025",
+        image: null,
+        text: "Einstein has a funny nose. What do you guys think about that?",
+        likes: 0,
+      },
+      {
+        authorImage: "img/pfp1.jpg",
+        date: "Oct 3, 2025",
+        image: "img/city.jpg",
+        text: "Look at the picture I made :O",
+        likes: 0,
+      },
+      {
+        authorImage: "img/pfp1.jpg",
+        date: "Oct 3, 2025",
+        image: null,
+        text: "So tired today. Gonna sleep.",
+        likes: 0,
+      },
+      {
+        authorImage: "img/pfp1.jpg",
+        date: "Oct 3, 2025",
+        image: null,
+        text: "Eat to much",
+        likes: 0,
+      },
+      {
+        authorImage: "img/pfp1.jpg",
+        date: "Oct 3, 2025",
+        image: null,
+        text: "Run my first 10k",
+        likes: 0,
+      },
+      {
+        authorImage: "img/pfp1.jpg",
+        date: "Oct 3, 2025",
+        image: null,
+        text: "I want to go home from work",
+        likes: 0,
+      },
+      {
+        authorImage: "img/pfp1.jpg",
+        date: "Oct 3, 2025",
+        image: null,
+        text: "Nice food",
+        likes: 0,
+      },
+      {
+        authorImage: "img/pfp1.jpg",
+        date: "Oct 3, 2025",
+        image: null,
+        text: "So tired today.",
+        likes: 0,
+      },
+    ],
+  },
   getters: {},
   mutations: {},
   actions: {},
