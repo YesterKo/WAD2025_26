f2ab2e37a7c68e8d1ea17a7b09e0c4f5cfc57214
Jesper Korsen <jesper.korsen@gmail.com>
2025-11-21 19:20:45 +0200

    Add PostsView layout

---
+++ src/views/PostsView.vue
@@ -0,0 +1,12 @@
+<div class="responsive-layout">
+  <aside class="left-aside">
+    <p>This is the left aside.</p>
+  </aside>
+
+<!-- TODO: add logic to use post component and populate it -->
+  <main id="posts-container"></main>
+
+  <aside class="right-aside">
+    <p>This is the right aside.</p>
+  </aside>
+</div>
