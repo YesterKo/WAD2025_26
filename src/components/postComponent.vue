c26ef663837fd3d949f8eb34968d76273b0578e7
Jesper Korsen <jesper.korsen@gmail.com>
2025-11-21 22:12:50 +0200

    Add PostComponent without templating

---
+++ src/components/postComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <article class="post">
+    <header>
+      <img src="img/pfp1.jpg" width="50" alt="The Author's profile picture" />
+      <p>Oct 3, 2025</p>
+    </header>
+    <img src="img/food.jpg" />
+    <p>Jippie food!</p>
+    <a href="#"><i class="fa-regular fa-thumbs-up"></i></a>
+  </article>
+</template>
