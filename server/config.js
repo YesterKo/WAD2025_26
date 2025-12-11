7b6bae28f1166fd721095e9a185047c93dfed7e7
Jesper Korsen <Jesper.Korsen@gmail.com>
2025-12-11 14:41:54 +0200

    Add Docker Compose and configure DB env vars

--- server/config.js
+++ server/config.js
@@ -1,12 +1,12 @@
-
-
 module.exports = {
   jwtSecret: "5Uy0awtZ44g",
   database: {
-    host: 'localhost',
-    database: "WAD202526",
-    user: "postgres",
-    password: "WAD202526",
-    port: 5432
-  }
+    host: process.env.POSTGRES_HOST || "localhost",
+    database: process.env.POSTGRES_DB || "WAD202526",
+    user: process.env.POSTGRES_USER || "postgres",
+    password: process.env.POSTGRES_PASSWORD || "WAD202526",
+    port: process.env.POSTGRES_PORT
+      ? parseInt(process.env.POSTGRES_PORT)
+      : 5432,
+  },
 };
